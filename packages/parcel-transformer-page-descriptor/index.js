145b6801415ca876a54be92c167a8e75a450f294
shooore <shooore@gmail.com>
2023-02-03 13:10:28 +0300

    dev

---
+++ packages/parcel-transformer-page-descriptor/index.js
@@ -0,0 +1,14 @@
+const { Transformer } = require('@parcel/plugin');
+const JSON5 = require('json5');
+
+module.exports = new Transformer({
+    async transform({ asset, options, resolve }) {
+        const source = await asset.getCode();
+        const page_descriptor = JSON5.parse(source);
+        let deps = typeof page_descriptor.contents === 'string' ? [page_descriptor.contents] : page_descriptor.contents;
+        for (let dep of deps) {
+            asset.addDependency({ specifier: dep });
+        }
+        return [asset];
+    },
+});
