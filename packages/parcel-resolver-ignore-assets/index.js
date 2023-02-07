f614e71ceb93af7b6ce164bf2e62dc2bc87641a7
shoore <shooore@gmail.com>
2023-02-07 19:44:35 +0300

    v1.0.0 RC3

--- packages/parcel-resolver-ignore-assets/index.js
+++ packages/parcel-resolver-ignore-assets/index.js
@@ -5,9 +5,12 @@ module.exports = new Resolver({
     async resolve(resolver_param) {
         const { pipeline, specifier, options: { env }, dependency } = resolver_param;
         const asset_abs_path = dependency.resolveFrom && resolvePath(dirname(dependency.resolveFrom), specifier.replace(/^~?\//, ''));
+        if (!specifier) {
+            return { isExcluded: true };
+        }
         if (asset_abs_path && env.localAssetsPath && asset_abs_path.includes(env.localAssetsPath) && !pipeline) {
             // console.log("+++ Resolver excludes: %o", specifier);
-            return { isExcluded: true }
+            return { isExcluded: true };
         }
         return null;
     },
