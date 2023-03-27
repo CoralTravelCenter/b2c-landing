eb03cc955125f733bedd34560433edfa294e08bc
shooore <shooore@gmail.com>
2023-03-27 13:11:12 +0300

    postinstall symlinking fix, 'dir' type tryout

--- bin/postinstall.mjs
+++ bin/postinstall.mjs
@@ -3,8 +3,8 @@
 import fs from "fs";
 
 if (!fs.existsSync('node_modules/parcel-resolver-ignore-assets')) {
-    fs.symlinkSync('../packages/parcel-resolver-ignore-assets', 'node_modules/parcel-resolver-ignore-assets');
+    fs.symlinkSync('../packages/parcel-resolver-ignore-assets', 'node_modules/parcel-resolver-ignore-assets', 'dir');
 }
 if (!fs.existsSync('node_modules/parcel-transformer-page-descriptor')) {
-    fs.symlinkSync('../packages/parcel-transformer-page-descriptor', 'node_modules/parcel-transformer-page-descriptor');
+    fs.symlinkSync('../packages/parcel-transformer-page-descriptor', 'node_modules/parcel-transformer-page-descriptor', 'dir');
 }
