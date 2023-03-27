0ee952ca96c0908c538f8d517fc280bddcd5a970
shooore <shooore@gmail.com>
2023-03-27 17:48:28 +0300

    parcel-resolver-ignore-assets Windows fix

--- packages/parcel-resolver-ignore-assets/index.js
+++ packages/parcel-resolver-ignore-assets/index.js
@@ -1,5 +1,6 @@
 const { Resolver } = require('@parcel/plugin');
 const { dirname, resolve: resolvePath } = require('path');
+const { pathToFileURL } = require('url');
 
 module.exports = new Resolver({
     async resolve(resolver_param) {
@@ -8,8 +9,8 @@ module.exports = new Resolver({
         if (!specifier) {
             return { isExcluded: true };
         }
-        if (asset_abs_path && env.localAssetsPath && asset_abs_path.includes(env.localAssetsPath) && !pipeline) {
-            // console.log("+++ Resolver excludes: %o", specifier);
+        if (asset_abs_path && env.localAssetsPath && pathToFileURL(asset_abs_path).pathname.includes(env.localAssetsPath) && !pipeline) {
+            console.log("+++ Resolver excludes: %o", specifier);
             return { isExcluded: true };
         }
         return null;
