698382f070ef1a5ae146d070a2a672a8eb0208ff
shooore <shooore@gmail.com>
2023-03-27 20:03:20 +0300

    global install works on Windoze at last!!!

--- packages/parcel-resolver-ignore-assets/index.js
+++ packages/parcel-resolver-ignore-assets/index.js
@@ -5,15 +5,12 @@ const { pathToFileURL } = require('url');
 module.exports = new Resolver({
     async resolve(resolver_param) {
         const { pipeline, specifier, options: { env }, dependency } = resolver_param;
-        console.log("--- pipeline: %o; specifier: %o", pipeline, specifier);
         const asset_abs_path = dependency.resolveFrom && resolvePath(dirname(dependency.resolveFrom), specifier.replace(/^~?[\/\\]/, ''));
         if (!specifier) {
             return { isExcluded: true };
         }
-        console.log("env.localAssetsPath: %o", env.localAssetsPath);
-        console.log("pathname: %o", asset_abs_path && pathToFileURL(asset_abs_path).pathname);
         if (asset_abs_path && env.localAssetsPath && pathToFileURL(asset_abs_path).pathname.includes(env.localAssetsPath) && !pipeline) {
-            console.log("+++ Resolver excludes: %o", specifier);
+            // console.log("+++ Resolver excludes: %o", specifier);
             return { isExcluded: true };
         }
         return null;
