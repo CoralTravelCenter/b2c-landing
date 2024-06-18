10a5943f753f6794acce3ce0634802acd93c1f9e
shooore <shooore@gmail.com>
2024-06-18 13:24:44 +0300

    command definition + some options added

--- bin/index.mjs
+++ bin/index.mjs
@@ -5,9 +5,11 @@ import { hideBin } from 'yargs/helpers';
 
 import * as serveCommand from '../lib/serve-command.mjs'
 import * as buildCommand from '../lib/build-command.mjs'
+import * as deployCommand from '../lib/deploy-command.mjs'
 
 const argv = yargs(hideBin(process.argv))
     .command(serveCommand)
     .command(buildCommand)
+    .command(deployCommand)
     .help()
     .argv;
